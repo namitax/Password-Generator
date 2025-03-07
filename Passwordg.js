39f64eb07bb235e438d2fb33152a152400b01a9f
namitax <namithapaul2003@gmail.com>
2025-03-07 06:04:27 +0000

    Password Generator

---
+++ Passwordg.js
@@ -0,0 +1,45 @@
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$^&*-=+?/";
+
+const generateButton = document.getElementById("generate-button");
+generateButton.addEventListener("click", generate);
+
+const result = document.getElementById("result");
+const copyButton = document.getElementById("copy-button");
+copyButton.addEventListener("click", copyPassword);
+
+function generate() {
+  let characters = "";
+  if (document.getElementById("lowercase-option").checked) {
+    characters += lowercase;
+  }
+  if (document.getElementById("uppercase-option").checked) {
+    characters += uppercase;
+  }
+  if (document.getElementById("number-option").checked) {
+    characters += numbers;
+  }
+  if (document.getElementById("special-option").checked) {
+    characters += symbols;
+  }
+
+  if (characters !== "") {
+    const length = Number(document.getElementById("password-length").value);
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const index = Math.floor(Math.random() * characters.length);
+      password += characters[index];
+    }
+
+    result.value = password;
+  }
+}
+
+function copyPassword() {
+  result.select();
+  result.setSelectionRange(0, result.value.length);
+  navigator.clipboard.writeText(result.value);
+  alert("Password Copied: " + result.value);
+}
